12477da3b57d774493cf8fc5c2068f38a8a300dc
Mamba369 <aliaksandrmarchuk369@gmail.com>
2024-02-27 03:32:21 +0100

    update with ConversationalRetrievalChain and ConversationBufferMemory

--- README.md
+++ README.md
@@ -1,41 +1,21 @@
 # Langchain-Chatbot
 
 ## Overview
-This project contains a simple chatbot built using Langchain library and tested via connecting to Mistral-7B-Instruct-v0.1 model from OpenAI running locally at LM studio. Future modifications are planned for this project in order to gain practival NLP knowledge.
+This project contains chatbot built using Langchain library and tested via connecting to Mistral-7B model running locally at LM studio. Future modifications described below are planned for this project in order to gain practival NLP and AI knowledge.
 
 ## Already Implemented
-- Start version of chatbot
+- Chatbot working in two modes: first in conversational manner with memory and second in retrieval manner without memory in order to test RAG performance
 
 ## Planned
-- Adding memory and RAG (Retrieval Augmented Generation)
-
-## Files
-- `app.py`: Main file with application code.
-- `requirements.txt`: Contains list of python dependencies required to run application.
+- Improve RAG with FLARE
 
 ## Usage
-To interact with chatbot, follow these steps:
-1. Install LM studio and run a recent model like Mistral 7B locally.
-2. Set up required parameters in main file.
-3. Run `app.py` file to initialize chatbot and interact with running model.
+To interact with chatbot locally, follow these steps:
+1. In LM studio load model like Mistral 7B and start local server.
+2. Run `app.py` file to initialize chatbot and interact with it.
 
 ## Example Usage
-
-System: Send a message to start chat!
-
-User: Hi, tell me please some ideas for python project related to NLP so I can learn this topic and also practice ?
-
-Mistral-7B-Instruct-v0.1: Hello! There are many interesting Python projects related to NLP that you could work on. Here are a few ideas to get you started:
-
-1. Sentiment analysis: Write a program to analyze the sentiment of a given text, such as whether it is positive, negative or neutral. You can use pre-built datasets, like the IMDb movie reviews dataset, or build your own by collecting data from the web.
-2. Text classification: Write a program to classify text into different categories based on its content. For example, you could create a program that classifies news articles as either "politics" or "sports".
-3. Named entity recognition: Write a program to identify named entities in text, such as people, organizations, and locations.
-4. Machine translation: Write a program to translate text from one language to another using machine learning algorithms.
-5. Text summarization: Write a program to automatically generate a summary of a given text.
-6. Chatbot development: Build a chatbot that can understand natural language and respond appropriately. You can use libraries like spaCy or NLTK to process the user's input and generate a response.
-7. Text-to-speech conversion: Write a program to convert written text into spoken words using machine learning algorithms.
-
-These are just a few ideas, but there are many other projects you could work on depending on your interests. I hope these ideas help you get started with NLP in Python!
+Conversational mode - conversation_example.png
 
 ## Author
 Aliaksandr Marchuk
--- app.py
+++ app.py
@@ -1,81 +1,158 @@
 import panel as pn
+import param
+from pathlib import Path
+from PyPDF2 import PdfReader
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.docstore.document import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import (
+    HuggingFaceBgeEmbeddings,
+    SentenceTransformerEmbeddings,
+)
+
+pn.extension()
 
 # Define parameters
-MODEL_NAME = "Mistral-7B-Instruct-v0.1"
+MODEL_NAME = "TheBloke/Mistral-7B-v0.1-GGUF"
 BASE_URL = "http://localhost:1234/v1"
 OPENAI_API_KEY = "not-needed"
-
-# Load Panel extension
-pn.extension()
+PARENT_FOLDER_PATH = Path(__file__).parent
+EMBEDDINS_MODEL_NAME = "all-MiniLM-L6-v2"
+CHUNK_SIZE = 200
+CHUNK_OVERLAP = 20
+BASIC_INPUT_PATH = "basic_test_input.txt"
 
 
 class Chatbot:
+    model_name: str = param.String(default=MODEL_NAME)
+    base_url: str = param.String(default=BASE_URL)
+    openai_api_key: str = param.String(default=OPENAI_API_KEY)
+    emdeddings: HuggingFaceBgeEmbeddings
+    documents: list[Document]
+
     def __init__(
         self,
-        model_name: str = MODEL_NAME,
-        base_url: str = BASE_URL,
-        openai_api_key: str = OPENAI_API_KEY,
+        is_locally: bool = True,
+        model_name: str = None,
+        base_url: str = None,
+        openai_api_key: str = None,
     ) -> None:
-        self.model_name = model_name
-        self.base_url = base_url
-        self.openai_api_key = openai_api_key
-        self.__create_llm_chain()
+        self.embeddings = SentenceTransformerEmbeddings(model_name=EMBEDDINS_MODEL_NAME)
+        self.documents = []
         self.__create_chat_interface()
 
-    def __create_llm_chain(self) -> None:
-        """
-        Create LLM chain for chatbot.
-        """
-        TEMPLATE = """<s>[INST] You are a friendly chat bot who's willing to help answer user:
-        {user_input} [/INST] </s>
-        """
-        prompt = PromptTemplate(template=TEMPLATE, input_variables=["user_input"])
+        if not is_locally:
+            if not (base_url and openai_api_key):
+                raise Exception(
+                    "In order to initialize chatbot using remote endpoint please provide 'base_url' and 'openai_api_key'"
+                )
+            elif not model_name:
+                raise Warning(
+                    f"Since 'model_name' is not provided, default one will be used, equals to {self.model_name}"
+                )
+            else:
+                self.model_name = model_name
+
+            self.base_url = base_url
+            self.openai_api_key = openai_api_key
+        else:
+            self._load_text_documents(BASIC_INPUT_PATH)
+
         self.llm = ChatOpenAI(
             model=self.model_name,
             base_url=self.base_url,
             openai_api_key=self.openai_api_key,
         )
-        self.llm_chain = LLMChain(llm=self.llm, prompt=prompt)
+
+    def _load_text_documents(self, file_paths: str | list[str]) -> None:
+        file_paths = [file_paths] if type(file_paths) is str else file_paths
+        for file_path in file_paths:
+            file_path = PARENT_FOLDER_PATH / file_path
+            print(file_path)
+            text_loader = TextLoader(file_path)
+            self.documents.extend(text_loader.load())
+
+        self.__initialize_retriever()
+
+    def _load_pdf_documents(self, file_paths: str | list[str]) -> None:
+        file_paths = [file_paths] if type(file_paths) is str else file_paths
+        for file_path in file_paths:
+            file_path = PARENT_FOLDER_PATH / file_path
+            with open(file_path, "rb") as file:
+                pdf_loader = PdfReader(file)
+                for page in pdf_loader.pages:
+                    if page_content := page.extract_text():
+                        self.documents.append(Document(page_content=page_content))
+
+        self.__initialize_retriever()
+
+    def __initialize_retriever(self):
+        print(
+            f"Number of Document objects before recursive splitting: {len(self.documents)}"
+        )
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP
+        )
+        self.documents = text_splitter.split_documents(self.documents)
+        vector_store = FAISS.from_documents(self.documents, self.embeddings)
+        self.retriever = vector_store.as_retriever(search_type="similarity")
+        print(
+            f"Number of Document objects after recursive splitting: {len(self.documents)}"
+        )
+
+    def __create_conversational_chain(self) -> None:
+        memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+        self.llm_chain = ConversationalRetrievalChain.from_llm(
+            llm=self.llm,
+            retriever=self.retriever,
+            memory=memory,
+            verbose=True,
+        )
+
+    def __create_retrieval_chain(self) -> None:
+        self.llm_chain = RetrievalQA.from_llm(
+            llm=self.llm,
+            retriever=self.retriever,
+            return_source_documents=True,
+            verbose=True,
+        )
 
     def __create_chat_interface(self) -> None:
-        """
-        Create a chat interface using Panel's chat components.
-        """
-        self.chat_interface = pn.chat.ChatInterface(callback=self.callback)
-
+        self.chat_input = pn.widgets.TextInput(placeholder="Ask questions here!")
+        self.chat_interface = pn.chat.ChatInterface(
+            callback=self.__chat_respond_callback,
+            sizing_mode="stretch_width",
+            widgets=[self.chat_input],
+            callback_exception="verbose",
+        )
         self.chat_interface.send(
-            "Send a message to receive an echo!", user="System", respond=False
+            "Send a message to start chatting!", user="System", respond=False
         )
         self.chat_interface.servable()
 
-    async def callback(
+    async def __chat_respond_callback(
         self, contents: str, user: str, instance: pn.chat.ChatInterface
-    ) -> None:
-        """
-        Callback function to handle user input.
+    ):
+        output_key = "answer" if self.is_conversational else "result"
+        response = await self.llm_chain.ainvoke(input=contents)
 
-        Args:
-            contents (str): User's message.
-            user (str): User's name.
-            instance (pn.chat.ChatInterface): Chat interface instance.
-        """
-        instance.send(
-            await self.llm_chain.apredict(user_input=contents),
-            user=self.llm.model_name,
-            respond=False,
-        )
+        yield {"user": MODEL_NAME, "value": response[output_key]}
 
-    def run(self) -> None:
-        """
-        Run chatbot.
-        """
+    def run(self, is_conversational: bool = False) -> None:
+        self.is_conversational = is_conversational
+        if self.is_conversational:
+            self.__create_conversational_chain()
+        else:
+            self.__create_retrieval_chain()
         self.chat_interface.show()
 
 
 if __name__ == "__main__":
-    # Create and run chatbot
     app = Chatbot()
-    app.run()
+    app.run(is_conversational=True)
